808265fa47dcd7253c99dcc7e6105ae4ee78c93a
claudiob <claudiob@gmail.com>
2011-06-02 19:45:40 -0700

    Added Facebook metadata

--- footer.php
+++ footer.php
@@ -1,17 +1,16 @@
-
-    <h1>F&iacute;n.</h1>
- 
-  </div>
-<!-- TODO: Add facebook and twitter -->
-<script type="text/javascript"> // Google Analytics
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-23662978-1']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-</body>
+      <h1>F&iacute;n.</h1>
+    </div>
+    <script type="text/javascript">
+    //<![CDATA[
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-23662978-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    //]]>
+    </script>
+  </body>
 </html>
--- header.php
+++ header.php
@@ -2,26 +2,22 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-
-<title>Boxoffice Script: del gui&oacute;n a la pel&iacute;cula</title>
-
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="Content-Language" content="es" />
-<meta name="language" content="es" />
-<meta name="robots" content="all" />
-<meta name="description" content="Ponemos en contacto guionistas emergentes con productoras de cine, convertimos guiones en pel&iacute;culas." />
-<meta name="keywords" content="movie, script, Spain, guion, escaparate, guionistas, Espana" />
-<meta name="viewport" content="width=900" />
-<link href="css/style.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="img/favicon_iphone.png" rel="apple-touch-icon" />
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-23662978-1']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-</head>
+  <title>Boxoffice Script: del gui&oacute;n a la pel&iacute;cula</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="Content-Language" content="es" />
+  <meta name="language" content="es" />
+  <meta name="robots" content="all" />
+  <meta name="description" content="Ponemos en contacto guionistas emergentes con productoras de cine, convertimos guiones en pel&iacute;culas." />
+  <meta name="keywords" content="movie, script, Spain, guion, gui&oacuten;, emergentes, escaparate, guionistas, Espana" />
+  <meta name="viewport" content="width=900" />
+  <meta name='og:title' content='Boxoffice Script' />
+  <meta name='og:type' content='website' />
+  <meta name='og:url' content='http://boxofficescript.com' />
+  <meta name='og:image' content='http://boxofficescript.com/img/boxofficescript_facebook_logo.png' />
+  <meta name='og:author' content='Pau Brunet' />
+  <meta name='og:site_name' content='Boxoffice Script' />
+  <meta name='fb:page_id' content='176734525715662' />
+  <meta name='fb:admins' content='665788321' />
+  <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="img/favicon_iphone.png" rel="apple-touch-icon" />
+  </head>
